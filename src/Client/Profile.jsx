d1a5205cc3cd2fb48d8f3c49a8fc2cde45aceb7a
Malak-o <77550791+Malak-o@users.noreply.github.com>
2022-10-26 00:44:02 +0200

    profile 2nd commit

--- src/Client/Profile.jsx
+++ src/Client/Profile.jsx
@@ -10,28 +10,28 @@ const Profile = () => {
                 <Fragment>
                     <metadata title={'Mina Sidor'} />
 
-                    <h2 class="mt-4 ml-4">Mina Sidor</h2>
-                    <div class="row justify-content-around mt-4 client-info">
-                        <div class="col-4 col-md-3">
-                            <figure class='avatar avatar-profile'>
-                                <img class="rounded-circle img-fluid" src='' alt='' />
+                    <h2 className="mt-4 ml-4">Mina Sidor</h2>
+                    <div className="row justify-content-around mt-4 client-info">
+                        <div className="col-4 col-md-3">
+                            <figure className='avatar avatar-profile'>
+                                <img className="rounded-circle img-fluid" src='' alt='' />
                             </figure>
                             <a href="#" id="edit_profile" class="btn btn-primary btn-block my-4">
                                 Redigera Profil
                             </a>
                         </div>
-                        <div class="col-4 col-md-5">
+                        <div className="col-4 col-md-5">
                             <h4>Fullständiga namn</h4>
                             <p>Testar Testsson</p>
                             
                             <h4>Epost adress</h4>
                             <p>TestTestsson@gmail.com</p>
 
-                            <a href="#" class="btn btn-danger btn-block mt-5">
+                            <a href="#" className="btn btn-danger btn-block mt-5">
                                 Mina ordrar
                             </a>
 
-                            <a href="#" class="btn btn-primary btn-block mt-3">
+                            <a href="#" className="btn btn-primary btn-block mt-3">
                                 Byt Lösenord
                             </a>
                         </div>

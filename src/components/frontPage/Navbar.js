e202c3544219992f936a68aa0576e6aa3f8cb134
rakelbjrk <sigurdardottir65>
2022-11-21 10:03:01 +0100

    fixat logo

--- src/components/frontPage/Navbar.js
+++ src/components/frontPage/Navbar.js
@@ -5,7 +5,7 @@ import {HiOutlineChevronUp} from 'react-icons/hi'
 import Login from '../login/Login';
 import './navBar/Navbar.css';
 import '../pages/homepage.css';
-import logo from '../assets/logoStadaFint.png';
+import logo from '../assets/städafint2.png';
 import Kontakt from '../pages/Kontakt';
 
 
@@ -17,7 +17,7 @@ const Navbar = () => {
     return(
       <>
               <nav className='nav'>
-            <img src={logo} className="logo" alt="cleaning-logo" />
+            <img src={logo} className="logo" alt="städafint2.png" />
       <ul>
         <li>
           <Link to="/">Hem</Link>

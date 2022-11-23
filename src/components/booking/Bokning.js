842e53a9f6eae05006fc38d51390f5bfcbd3e8fd
dyras-school <johan.forsberg@my.com>
2022-11-23 12:27:17 +0100

    Fixar så att bokningshistoriken funkar igen

--- src/components/booking/Bokning.js
+++ src/components/booking/Bokning.js
@@ -1,92 +1,36 @@
-import { useState } from "react";
-import React from "react";
-import './Booking.css';
-import { parse } from "@fortawesome/fontawesome-svg-core";
-
 const Bokning = (props) => {
 
-    const [time, setTime] = useState("");
-    const [date, setDate] = useState("");
-    const [optionalMessage, setMessage] = useState("");
-    const [user, setUser] = useState("");
-    const [userId, setUserId] = useState("");
+    const { bokning, setBokning, user } = props;
 
-    const handleSave = async (event) => {
-        event.preventDefault()
+    const handleDelete = async (id) => {
 
-     
-        
-        try {
-            const object = JSON.parse(localStorage.getItem("loggedInUser"));
-            if (object) {
-                setUser(object);
-                console.log(object)
-                var id = user.id;
-                
-                setUserId(id);
-                console.log(id);
-            }else{
-                setUserId(0);
-            }
-            
-
-
-        }catch{
-            console.log("Där gick något fel")
-            console.log(currentUser);
-            console.log(parsedUser);
-
-        }
-
-        await fetch(`${process.env.REACT_APP_BASE_URL}/api/booking`, {
-            method: 'POST',
+        await fetch(`${process.env.REACT_APP_BASE_URL}/api/booking/${id}`, {
+            method: 'DELETE',
             headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                time: time,
-                date: date,
-                optionalMessage: optionalMessage,
-                id: 1
-            })
+                Authorization: `Bearer ${user.token}`
+            }
         })
+
+        let response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/booking`, {
+            headers: {
+                Authorization: `Bearer ${user.token}`
+            }
+        })
+        let bokningar = await response.json()
+
+        console.log("bokningar", bokningar)
+
+        setBokning(bokningar)
+
     }
 
     return (
-        <div className="headerContainer">
-            <div className="headerContainer h1">
-                <h1>Lägg till bokning</h1>
-                </div>
-        <form>
-            <p>Datum</p>
-            <input
-            type="text"
-            placeholder="2023-05-13"
-            onChange={(e) => setDate(e.target.value)}
-            />
-        </form>
-
-        <form>
-            <p>Klockslag</p>
-            <input
-            type="text"
-            placeholder="15:33"
-            onChange={(e) => setTime(e.target.value)}
-            />
-        </form>
-
-        <form>
-            <p>Meddelande till städaren</p>
-            <input
-            type="text"
-            placeholder="Valfritt meddelande"
-            onChange={(e) => setMessage(e.target.value)}
-            />
-        </form>
-        <button onClick={handleSave}>Boka tid</button>
-
+        <div>
+            <h2>{bokning.date}</h2>
+            <p>{bokning.time}</p>
+            <button onClick={() => handleDelete(bokning.id)}>Avboka</button>
         </div>
     )
 }
 
-export default Bokning
+export default Bokning;
